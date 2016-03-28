ba3a0cf9e4feec568f47df3a0561cb433bc4af1f
Sqrt of Saturn <sqrt@octoblu.com>
2016-03-28 11:18:41 -0700

    it is alive!!

--- build.sh
+++ build.sh
@@ -1,1 +1,47 @@
 #!/bin/bash
+
+print_usage(){
+  echo "Usage: ./build.sh <connector-name> <tag>"
+}
+
+create_directories(){
+  local connector_name="$1"
+  local tag="$2"
+  mkdir -p "deploy/${connector_name}/latest"
+  mkdir -p "deploy/${connector_name}/${tag}"
+}
+
+move_connector_to_deploy(){
+  local connector_name="$1"
+  local tag="$2"
+  rsync -av * "deploy/${connector_name}/latest" --exclude deploy --exclude .git
+  rsync -av * "deploy/${connector_name}/${tag}" --exclude deploy --exclude .git
+}
+
+main() {
+  if [ "$1" == "--help" -o "$1" == "-h" -o "$1" == "help" -o "$1" == "-?" ]; then
+    print_usage
+    exit 1
+  fi
+
+  if [ -z "$1" ]; then
+    print_usage
+    echo "Missing connector name"
+    exit 1
+  fi
+
+  if [ -z "$2" ]; then
+    print_usage
+    echo "Missing tag"
+    exit 1
+  fi
+
+  local connector_name="$1"
+  local tag="$2"
+
+  create_directories "$connector_name" "$tag"
+  move_connector_to_deploy "$connector_name" "$tag"
+  exit 0
+}
+
+main $@

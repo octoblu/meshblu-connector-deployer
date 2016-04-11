9dbdb208b0c22c9e88d302b93c07e6843355539b
Sqrt of Saturn <sqrt@octoblu.com>
2016-04-11 15:14:16 -0700

    v6.0.10 remove delete prefix

--- travis_install_node.sh
+++ travis_install_node.sh
@@ -2,7 +2,7 @@
 
 install_node(){
   echo "### Installing node"
-  nvm install --delete-prefix $PACKAGER_NODE_VERSION
+  nvm install $PACKAGER_NODE_VERSION
   BASE_URL=$(node -p "v=parseInt(process.versions.node),(v>=1&&v<4?'https://iojs.org/dist/':'https://nodejs.org/dist/')+process.version")
   X86_FILE=$(node -p "v=parseInt(process.versions.node),(v>=1&&v<4?'iojs':'node')+'-'+process.version+'-'+process.platform+'-x86'")
   if [[ "$ARCH" == "386" ]]; then

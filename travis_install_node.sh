7a612012bfe3ba4d0da3ce0d5441895f86edee6d
Sqrt of Saturn <sqrt@octoblu.com>
2016-04-12 11:03:40 -0700

    v6.0.15 fixed nvm?

--- travis_install_node.sh
+++ travis_install_node.sh
@@ -2,28 +2,27 @@
 
 install_node(){
   echo "### Installing node"
-  nvm install $PACKAGER_NODE_VERSION
-  nvm use --delete-prefix $PACKAGER_NODE_VERSION
-  BASE_URL=$(node -p "v=parseInt(process.versions.node),(v>=1&&v<4?'https://iojs.org/dist/':'https://nodejs.org/dist/')+process.version")
-  X86_FILE=$(node -p "v=parseInt(process.versions.node),(v>=1&&v<4?'iojs':'node')+'-'+process.version+'-'+process.platform+'-x86'")
   if [[ "$PACKAGER_ARCH" == "386" ]]; then
+    echo "### using 386 node installation method"
+    BASE_URL=$(node -p "v=parseInt(process.versions.node),(v>=1&&v<4?'https://iojs.org/dist/':'https://nodejs.org/dist/')+process.version")
+    X86_FILE=$(node -p "v=parseInt(process.versions.node),(v>=1&&v<4?'iojs':'node')+'-'+process.version+'-'+process.platform+'-x86'")
     pushd "/tmp"
       echo "### Download node for x86"
       wget $BASE_URL/$X86_FILE.tar.gz;
       tar -xf $X86_FILE.tar.gz;
       export PATH=$X86_FILE/bin:$PATH;
     popd
+  else
+    echo "### using amd64 node installation method"
+    nvm install $PACKAGER_NODE_VERSION
+    nvm use --delete-prefix $PACKAGER_NODE_VERSION
   fi
 }
 
 install_nvm(){
   echo "### Install nvm"
-  rm -rf ~/.nvm
-  git clone https://github.com/creationix/nvm.git ~/.nvm
-  pushd ~/.nvm
-    git checkout `git describe --abbrev=0 --tags`
-  popd
-  source ~/.nvm/nvm.sh
+  git clone https://github.com/creationix/nvm.git /tmp/.nvm
+  source /tmp/.nvm/nvm.sh
 }
 
 setup(){
@@ -31,6 +30,8 @@ setup(){
   if [[ $TRAVIS_OS_NAME == "linux" ]]; then
     export CXX=g++-4.8;
   fi
+  echo "### Verifying CXX"
+  $CXX --version
 }
 
 verify(){
@@ -38,8 +39,6 @@ verify(){
   npm --version
   echo "### Verifying node"
   node --version
-  echo "### Verifying CXX"
-  $CXX --version
 }
 
 main(){
